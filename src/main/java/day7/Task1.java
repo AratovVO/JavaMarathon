ec0d42633732f7651cf9c08f12e042905bbbdbe5
AratovVO <aratov.valera2010@yandex.ru>
2023-07-25 20:06:50 +0400

    added task1 for day 7

---
+++ src/main/java/day7/Task1.java
@@ -0,0 +1,10 @@
+package day7;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        Airplane airplane1 = new Airplane("Boeing 747", 1969, 71, 183500);
+        Airplane airplane2 = new Airplane("Airbus", 1969, 80, 183500);
+        Airplane.compareAirplanes(airplane2, airplane1);
+    }
+}
