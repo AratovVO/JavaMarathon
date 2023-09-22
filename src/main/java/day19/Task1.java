df404e4abc7727c21c4ed339ef1dfa4110525f1f
Aratov Valery <aratov.valera2010@yandex.ru>
2023-09-22 14:28:08 +0400

    fixed task 1 for day 19

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -4,27 +4,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-
 /**
  * @author Neil Alishev
  */
 public class Task1{
     public static void main(String[] args)  {
-        File file = new File("C:\\IdeaProjects\\JavaMarathon2021\\src\\main\\resources\\dushi.txt");
+        File file = new File("C:\\Users\\v.aratov\\Desktop\\Aratov\\JavaMarathon2021\\src\\main\\resources\\dushi.txt");
         HashMap<String,Integer> hm  = new HashMap<>();
         try {
             Scanner scanner = new Scanner(file);
             scanner.useDelimiter("[.,:;()?!\"\\s–]+");
-            int counter = 1;
+            int counter;
             while(scanner.hasNext()){
                 String s = scanner.next();
                 if(s == null){
                     throw new EmptyStackException();
                 }
                 if(hm.containsKey(s)){
+                    counter = hm.get(s);
                     counter++;
                     hm.replace(s,hm.get(s),counter);
                 } else {
+                    counter = 1;
                     hm.put(s,counter);
                 }
             }
@@ -39,7 +40,7 @@ public class Task1{
             while(counter != (entryList.size()-100)){
                 System.out.println(entryList.get(counter--));
             }
-            System.out.println(hm.get("Чичиков"));
+// -------------------------------- Чичиков - 601 --------------------------------------------------
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -48,5 +49,4 @@ public class Task1{
         }
 
     }
-
 }
